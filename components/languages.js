5c1f16257bf037f2c743f24cd272aeca601be472
Solanki Gaurav <solankigaurav2107@gmail.com>
2021-12-25 17:40:57 +0530

    star ratings

--- components/languages.js
+++ components/languages.js
@@ -1,19 +1,30 @@
+import StarRatings from "react-star-ratings";
 import data from "../public/assets/titleData.json";
 function Languages() {
   return (
     <div className="flex justify-center">
       {data.map((d, ind) => {
         return (
-          <div key={ind} className="rounded-lg shadow-lg bg-white max-w-sm p-6 mr-8">
-              <img
-                className="rounded-t-lg h-60 w-60 mx-auto object-center"
-                src={d.img}
-                alt=""
-              />
+          <div
+            key={ind}
+            className="rounded-lg shadow-lg bg-white max-w-sm p-6 mr-8"
+          >
+            <img
+              className="rounded-t-lg h-60 w-60 mx-auto object-center"
+              src={d.img}
+              alt=""
+            />
             <div className="p-6">
-              <h5 className="text-gray-900 text-xl font-medium mb-2">{d.title}</h5>
-              <p className="text-gray-700 text-base mb-4">{d.description}
-              </p>
+              <h5 className="text-gray-900 text-3xl font-bold mb-2">
+                {d.title}
+              </h5>
+              <StarRatings
+                  rating={d.rating}
+                  starRatedColor="gold"
+                  starDimension="40px"
+                  starSpacing="6px"
+                />
+              <p className="text-gray-700 text-base mb-4 mt-6">{d.description}</p>
             </div>
           </div>
         );
--- components/projects.js
+++ components/projects.js
@@ -5,8 +5,11 @@ function Projects() {
     <div className="flex justify-center">
       {data.map((d, ind) => {
         return (
-          <div key={ind} className="rounded-lg shadow-lg bg-white max-w-sm p-6 mr-8">
-              <a key={ind} href={d.link} target="_blank" rel="noreferrer">
+          <div
+            key={ind}
+            className="rounded-lg shadow-lg bg-white max-w-sm p-6 mr-8"
+          >
+            <a key={ind} href={d.link} target="_blank" rel="noreferrer">
               <img
                 className="rounded-t-lg h-60 m-0 object-center"
                 src={d.img}
@@ -15,11 +18,11 @@ function Projects() {
               <div className="p-6">
                 <h5 className="text-gray-900 text-xl font-medium mb-2">
                   {d.title}
-                </h5>
+                </h5>                
                 <p className="text-gray-700 text-base mb-4">{d.description}</p>
               </div>
-          </a>
-            </div>
+            </a>
+          </div>
         );
       })}
     </div>
